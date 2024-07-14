acf5c38c2e07c15110b90f3841a144ad036ea5bf
josefreitas788 <josefreitas788@gmail.com>
2024-07-14 20:10:54 -0300

    Add user creation logging and error handling

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,30 +2,34 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/josefilo/caraCracha/internal/entity"
+	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
 )
 
 type UserRepositoryMongo struct {
 	collection *mongo.Collection
 }
 
-func NewUserRepositoryMongo(collection *mongo.Collection) *UserRepositoryMongo {
+func NewUserRepositoryMongo(collection *mongo.Collection) entity.IUserRepository {
 	return &UserRepositoryMongo{
 		collection: collection,
 	}
 }
 
 func (r *UserRepositoryMongo) CreateUser(ctx context.Context, user *entity.User) (*entity.User, error) {
-	_, err := r.collection.InsertOne(ctx, user)
+	InsertResult, err := r.collection.InsertOne(ctx, user)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "insert user failed")
 	}
+	zap.L().Info(fmt.Sprintf("User created with ID: %s", InsertResult.InsertedID))
 	return user, nil
 }
 
-func (r *UserRepositoryMongo) GetUser(ctx context.Context, id uint) (*entity.User, error) {
+func (r *UserRepositoryMongo) GetUser(ctx context.Context, id string) (*entity.User, error) {
 	user := &entity.User{}
 	err := r.collection.FindOne(ctx, entity.User{ID: id}).Decode(user)
 	if err != nil {
--- internal/repository/user_integration_test.go
+++ internal/repository/user_integration_test.go
@@ -1,1 +1,70 @@
 package repository
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-faker/faker/v4"
+	"github.com/josefilo/caraCracha/config/db"
+	"github.com/josefilo/caraCracha/internal/entity"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUserRepository(t *testing.T) {
+	// Setup
+	is := require.New(t)
+	ctx := context.Background()
+	databaseName := "caraCrach"
+	collectionName := "user"
+	db, err := db.NewMongoDBCollection(databaseName, collectionName)
+	is.Nil(err)
+	userRepository := NewUserRepositoryMongo(db)
+
+	t.Run("CreateUser", func(t *testing.T) {
+		// Given
+		is := require.New(t)
+		user := &entity.User{
+			ID:        faker.UUIDDigit(),
+			Email:     faker.Email(),
+			Password:  faker.Password(),
+			Name:      faker.Name(),
+			BirthDate: faker.Date(),
+			CreatedAt: faker.Date(),
+			UpdatedAt: faker.Date(),
+		}
+
+		// When
+		createdUser, err := userRepository.CreateUser(context.Background(), user)
+
+		// Then
+		is.Nil(err)
+		is.NotNil(createdUser)
+		is.Equal(user.Email, createdUser.Email)
+		is.Equal(user.Password, createdUser.Password)
+	})
+
+	t.Run("CreateUser failed", func(t *testing.T) {
+		// Given
+		is := require.New(t)
+		user := &entity.User{
+			ID:        faker.UUIDDigit(),
+			Email:     faker.Email(),
+			Password:  faker.Password(),
+			Name:      faker.Name(),
+			BirthDate: faker.Date(),
+			CreatedAt: faker.Date(),
+			UpdatedAt: faker.Date(),
+		}
+
+		// When
+		createdUser, err := userRepository.CreateUser(ctx, user)
+		is.Nil(err)
+		createdUser.ID = faker.UUIDDigit()
+		_, err = userRepository.CreateUser(ctx, createdUser)
+
+		// Then
+		//fmt.Println("Error: ", err.Error())
+		is.NotNil(err)
+		is.Contains(err.Error(), "user index: _email_1 dup key")
+	})
+}
